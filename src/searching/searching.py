0d26c65c5e02b81640f9adb7bfec5d10d7f639e6
Will Ediger <will.ediger@gmail.com>
2020-06-03 21:41:30 -0500

    binary search implementation

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,7 +8,18 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (high + low) // 2
+        guess = arr[mid]
+        if guess == target:
+            return mid
+        elif guess > target:
+            high = mid - 1
+        elif guess < target:
+            low = mid + 1
 
 
     return -1  # not found
